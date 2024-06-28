a418934f2342102c30de35ee7c77e0ea9185c4b9
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-28 16:10:54 +0200

    feat: retrieve all projects from backend API and log results to console

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,21 @@
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            projects: [],
+        }
+    },
+    created() {
+        axios
+            .get("http://127.0.0.1:8000/api/projects/")
+            .then((resp) => {
+                this.projects = resp.data.results;
+                // stampo i risultati della chiamata API nella console
+                console.log(this.projects);
+            });
+    },
 }
 </script>
 
