4cdb854ddb24e9c27130edd4f71eebef93954425
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-28 15:56:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <!-- <h1>Questo è App.vue</h1> -->
+  <AppMain />
+
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <h1>Questo è AppMain.vue</h1>
+</template>
+
+<style lang="scss" scoped></style>
