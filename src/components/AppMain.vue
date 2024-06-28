676753abe8aaaa7cba75230db69811d75e79d410
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-28 17:21:06 +0200

    feat: create ProjectCard component for project cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
 
 export default {
     data() {
@@ -13,14 +14,28 @@ export default {
             .then((resp) => {
                 this.projects = resp.data.results;
                 // stampo i risultati della chiamata API nella console
-                console.log(this.projects);
+                // console.log(this.projects);
             });
+    },
+    components: {
+        ProjectCard,
     },
 }
 </script>
 
 <template>
-    <h1>Questo è AppMain.vue</h1>
+
+    <div class="container">
+        <h1 class="py-3 text-center fw-bold">Elenco Progetti</h1>
+        <div class="row row-cols-1 row-cols-md-2 g-3">
+            <div class="col" v-for="project in projects">
+                <!-- Card Project -->
+                <ProjectCard :projectObj="project" />
+                <!-- /Card Project -->
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        projectObj: Object,
+    },
+    data() {
+        imageBaseUrl: 'http://127.0.0.1:8000/storage'
+    },
+}
+</script>
+
+<template>
+    <div class="card h-100 w-100" style="width: 18rem;">
+        <img :src="projectObj.thumb ? `${imageBaseUrl}/${projectObj.thumb}` : `https://placehold.co/300x150?text=Anteprima+non+disponibile`"
+            class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ projectObj.title }}</h5>
+            <p class="card-text">{{ projectObj.description }}</p>
+            <h6 class="me-2 fw-bold d-inline-block">Tipo: </h6>
+            <span>ciao</span>
+            <br>
+            <h6 class="me-2 fw-bold d-inline-block">Tecnologie:</h6>
+            <span>ciao</span>
+            <div class="align-self-end mt-3">
+                <a href="#" class="btn btn-success btn-sm">Dettagli</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
