8fdb12f4258f83d2a763f38247d11dc1379eda66
andrea-bertaglia <andreabertaglia@gmail.com>
2024-07-01 17:16:57 +0200

    feat: home and about-us draft layout

--- src/pages/AppAboutUs.vue
+++ src/pages/AppAboutUs.vue
@@ -5,7 +5,40 @@ export default {
 </script>
 
 <template>
-    <h1>Questa è About Us</h1>
+    <div class="container">
+        <h1 class="py-3 text-center fw-bold">About Us</h1>
+        <div class="text-center">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, molestias tempore. Consequatur qui, nisi
+                perferendis, tenetur iste deleniti sint praesentium nihil in id nulla neque ratione quam temporibus
+                voluptates eaque.
+                Nihil optio maxime omnis eaque odit veritatis sequi ratione quis culpa, sunt ex eos voluptatibus commodi
+                delectus modi deserunt tempora sint voluptates quae odio. Repellat nostrum animi in porro. Sunt.
+                Quis, eos soluta! Officiis cumque sint ex ratione commodi maxime eius similique doloremque, atque, optio
+                recusandae itaque velit accusamus pariatur obcaecati adipisci vero iste explicabo quisquam eveniet!
+                Repellendus, soluta est!
+                Nihil atque minima deserunt necessitatibus labore vero veniam quibusdam ab. Qui autem ex voluptatem
+                deleniti, assumenda facilis eaque error? Iusto unde adipisci quam doloribus ad. Odio deleniti
+                perferendis illo esse.
+                Sint, nam a consequatur iusto architecto quisquam provident, ab ea ipsa molestias voluptate nostrum
+                beatae fugit? Pariatur eos, dolor ipsa, fugit perspiciatis, accusamus quidem asperiores beatae quis
+                necessitatibus fuga doloremque.
+                Maiores, voluptatem! Molestias repellat mollitia ea delectus dolore amet iusto eligendi aut temporibus
+                id minima possimus, fuga sint nobis, hic, doloremque facilis soluta repudiandae quo nulla earum incidunt
+                facere pariatur.
+                Magnam magni corporis placeat, illum eaque quod ipsam error quia aliquid officiis ab, sunt molestiae
+                repellendus! Deleniti at porro non accusamus quae repellat laudantium earum eum suscipit. Cum, eius
+                totam?
+                Perspiciatis hic ipsam delectus corporis. Odit aperiam consectetur provident nisi voluptatum porro.
+                Aspernatur minus, quis nostrum maiores necessitatibus animi sit illum placeat ea sint quos eaque velit
+                cum fugiat quas!
+                Temporibus ullam rerum explicabo provident dolore! Excepturi doloribus aut adipisci sed voluptas
+                recusandae quisquam et consectetur dolore nulla ex autem facilis doloremque eos id voluptatibus,
+                nesciunt explicabo nostrum atque deleniti!
+                Repellat, ad? Accusantium, voluptatum labore optio dolore eligendi obcaecati unde ad. Fuga quidem magni
+                rem provident aspernatur officiis repudiandae doloremque, ea rerum voluptate iusto quasi, voluptatum
+                velit sed hic? Architecto!</p>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -5,7 +5,14 @@ export default {
 </script>
 
 <template>
-    <h1>Questa è Home Page</h1>
+    <div class="container">
+        <h1 class="py-3 text-center fw-bold">Home Page</h1>
+        <div class="text-center">
+            <img src="https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/Boolean/original.png?1623187562"
+                class="main-img rounded rounded-circle me-2 border" alt="Boolfolio"
+                style="max-width: 400px; margin-top: 2em;">
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
